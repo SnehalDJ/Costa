74787d1ef0444f920f39226c282adf1d43ab5f0e
SnehalDJ <snehal.jadhav@sage.com>
2019-09-09 17:04:29 +0100

    fixed loan creation

--- force-app/main/default/aura/LoanApplicationForm/LoanApplicationFormHelper.js
+++ force-app/main/default/aura/LoanApplicationForm/LoanApplicationFormHelper.js
@@ -9,12 +9,12 @@
 		action.setCallback(this, function(response){
 			var state = response.getState();
 			console.log("Create loan: " + JSON.stringify(state));
-			if(state === SUCCESS){
+			// if(JSON.stringify(state) === SUCCESS){
 				var loans = component.get("v.loans");
 				loans.push(response.getReturnValue());
 				component.set("v.loans", loans);
 				console.log("loan created in helper");
-			}
+			// }
 		});
 		$A.enqueueAction(action);
 	}
